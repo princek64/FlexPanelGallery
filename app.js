4590d648f1aa22351a99ecbda0d4231a9d656049
princek64 <princekukreja64@gmail.com>
2020-07-13 23:28:55 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const panel = document.querySelectorAll('.panel')
+
+
+function toggleOpen() {
+    this.classList.toggle('open')
+}
+
+
+function toggleActive(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+}
+
+panel.forEach(pane => {
+    pane.addEventListener('click', toggleOpen)
+})
+
+panel.forEach(pane => {
+    pane.addEventListener('transitionend', toggleActive)
+})
